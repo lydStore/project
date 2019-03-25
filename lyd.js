c576528a749717f59b02ccfc8664bde8802b077c
unknown <liyadong@kingnet.sh>
2019-03-25 13:46:51 +0800

    update

--- lyd.js
+++ lyd.js
@@ -19,4 +19,60 @@ function setTime(obj) {
   }, 1000);
 }
 
-//
+//获取url参数值的两种方式
+function getQueryName(name) {
+  //获取url的的参数
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+    return unescape(r[2]);
+  } else {
+    return null;
+  }
+}
+
+//滚动到摸个位置
+function scrollToLocal(h, durition) {
+  //h int，文档滚动到距离顶部的高度
+  durition = isNaN(durition) ? 200 : durition;
+  $("html,body")
+    .stop()
+    .animate({ scrollTop: h }, durition);
+}
+
+//通用请求方法，例：var data = request('/app/task/reward');
+function aldRequest(uri) {
+  var args = {};
+  var callback = "";
+  if (arguments.length >= 3) {
+    //3参数(有args,callback)
+    args = arguments[1];
+    callback = arguments[2];
+  } else if (arguments.length >= 2) {
+    //2参数(有args)
+    args = arguments[1];
+    callback = "";
+  }
+  $.post(
+    uri,
+    args,
+    function(data) {
+      if (data.ret > 0) {
+        if (data.ret == 1) {
+          //未登录
+          alert("请登录");
+        } else {
+          alert(data.msg);
+        }
+      } else {
+        //0请求成功
+        if (typeof callback === "function") {
+          callback(data.data);
+        } else {
+          alert(callback);
+        }
+      }
+    },
+    "json"
+  );
+}
