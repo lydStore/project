13fe81f9621a3b27b02b7024022b7bbdb191a61d
unknown <liyadong@kingnet.sh>
2019-03-28 13:02:16 +0800

    update

---
+++ reacttest/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+import menuConfig from "../../router/menuConfig";
+
+export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: window.location.pathname
+    };
+  }
+  //切换当前样式
+  changeUrl = () => {
+    this.setState({
+      path: window.location.pathname
+    });
+  };
+  render() {
+    return (
+      <div className="footer">
+        {menuConfig.map((item, index) => {
+          return (
+            <Link
+              onClick={this.changeUrl}
+              key={index}
+              to={item.path}
+              className={
+                window.location.pathname === item.path ? "current" : ""
+              }
+            >
+              {item.name}
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
+}
--- reacttest/src/index.js
+++ reacttest/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import * as serviceWorker from "./serviceWorker";
+import "./static/js/rem";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
