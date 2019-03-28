2d1c06ffac27f707b0c0975e3776ca15cabfb484
unknown <liyadong@kingnet.sh>
2019-03-28 10:24:12 +0800

    update03228

--- reacttest/src/app.js
+++ reacttest/src/app.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import router from "./router";
-export default class App extends Component {
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          {router.map((route, key) => {
-            return (
-              <Route
-                exact
-                key={key}
-                path={route.path}
-                component={route.component}
-              />
-            );
-          })}
-          <Footer />
-        </Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
+
+export default App;
