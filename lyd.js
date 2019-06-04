d426f4127f0e687e6a5e4b717b0c4e1d1f728642
李亚东 <46667329+lydStore@users.noreply.github.com>
2019-06-04 13:34:46 +0800

    Update lyd.js

--- lyd.js
+++ lyd.js
@@ -31,7 +31,18 @@ function getQueryName(name) {
     return null;
   }
 }
-
+function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+            theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
+}
 //滚动到某个位置
 function scrollToLocal(h, durition) {
   //h int，文档滚动到距离顶部的高度
