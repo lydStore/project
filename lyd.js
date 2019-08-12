0970d1d7803b9543a10bcca04f2b86fcc520ac8e
李亚东 <46667329+lydStore@users.noreply.github.com>
2019-08-12 17:30:24 +0800

    Update lyd.js

--- lyd.js
+++ lyd.js
@@ -1,8 +1,34 @@
 static = {
   exp: {
             phone: /^((0\d{2,3}-\d{7,8})|(1\d{10}))$/,
-            idcard: /^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/,
-            email: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/
+            email: /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/,
+            idcard: function (code){
+                  //18位身份证需要验证最后一位校验位
+                  if(code.length == 18){
+                      code = code.split('');
+                      //∑(ai×Wi)(mod 11)
+                      //加权因子
+                      var factor = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ];
+                      //校验位
+                      var parity = [ 1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2 ];
+                      var parity_new = [ 1, 0, 'x', 9, 8, 7, 6, 5, 4, 3, 2 ];
+                      var sum = 0;
+                      var ai = 0;
+                      var wi = 0;
+                      for (var i = 0; i < 17; i++)
+                      {
+                          ai = code[i];
+                          wi = factor[i];
+                          sum += ai * wi;
+                      }
+                      if((parity[sum % 11] != code[17]) && (parity_new[sum % 11] != code[17])){
+                          return false;
+                      }
+                      return true;
+                  }
+                  return false;
+          },
+         realname: /^[\u4e00-\u9fa5]{2,4}$/,//真实姓名
         },
 }
 
